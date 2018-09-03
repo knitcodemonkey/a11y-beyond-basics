f0b16bff21df2e1ae9d6b0cf28ee159f40b8f162
Jen Luker <jluker@deseretdigital.com>
2018-09-03 17:14:56 -0600

    Adding more files

--- presentation/slides/documentation/index.js
+++ presentation/slides/documentation/index.js
@@ -2,5 +2,5 @@ export { default as DocQuote1 } from "./quote";
 export { default as PastSelf } from "./past-self";
 export { default as DevEvo1 } from "./dev-evo1";
 export { default as DevEvo2 } from "./dev-evo2";
-export { default as AccessibleExamples } from "./past-self";
-export { default as DocQuote2 } from "./past-self";
+export { default as AccessibleExamples } from "./accessible-examples";
+export { default as DocQuote2 } from "./quote2";
